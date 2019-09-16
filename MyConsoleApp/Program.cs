78f3b74f0df4f9a1d8ae77980cbbe3652b8dd512
Melchsedech Wanga <melchwanga@gmail.com>
2019-09-16 12:51:43 +0300

    Initial commit

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using MyLinkedList;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] Letters = { 'E', 'B', 'E', 'E', 'B', 'A', 'B' };
+            int UserOption;
+            Console.WriteLine("Please press 1 use custom list or any other key to use the default LinkedList");
+            UserOption = Convert.ToInt32(Console.ReadLine());
+
+            if(UserOption == 1)
+            {
+                Console.WriteLine("Please key in the linked list nodes, one after another. Each value being a character between A-Z");
+                string InputLetters = Console.ReadLine();
+             
+                while(!Regex.IsMatch(InputLetters, @"^[a-zA-Z]+$"))
+                {
+                    Console.WriteLine("Invalid input detected, only letters with no spaces are accepted.");
+                    Console.WriteLine("Please key in the right input and hit Enter...");
+                    InputLetters = Console.ReadLine();
+                }
+                Letters = InputLetters.ToCharArray();
+            }            
+
+            LinkedList<char> MyList = new LinkedList<char>(Letters);
+
+            LinkedList<char> MyNewList = new LinkedList<char>();            
+            
+            //Output node frequency for the original list
+            MyLinkedList.LinkedListHelpers.TestNodeCount(MyList, "Original LinkedList");
+
+            MyNewList = MyLinkedList.LinkedListHelpers.RefineDuplicates(MyList);
+            
+            //Output node frequency for the refined list
+            MyLinkedList.LinkedListHelpers.TestNodeCount(MyNewList, "Refined LinkedList");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyLinkedList/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLinkedList
+{
+    public static class LinkedListHelpers
+    {
+       
+        /**
+         *  Function to output node frequency in a given Linked List
+         * 
+         */
+        public static void TestNodeCount(LinkedList<char> MyList, string Title)
+        {
+            if (MyList != null)
+            {
+
+                Dictionary<char, int> CountedNodes = new Dictionary<char, int> { };
+
+                LinkedListNode<char> CurrentNode = MyList.First;
+
+                while (CurrentNode != null)
+                {
+                    if (!CountedNodes.ContainsKey(CurrentNode.Value))
+                    {
+                        CountedNodes.Add(CurrentNode.Value, 1);
+                    }
+                    else
+                    {
+                        CountedNodes[CurrentNode.Value] = CountedNodes[CurrentNode.Value] + 1;
+                    }
+                    CurrentNode = CurrentNode.Next;
+                }
+                //Output the count results for each 
+                Console.WriteLine("*****LINKED LIST NODE FREQ: " + Title + "****");
+
+                foreach (KeyValuePair<char, int> item in CountedNodes)
+                {
+                    Console.WriteLine("Node: {0}, Frequency: {1}", item.Key, item.Value);
+                }
+            }
+        }
+        /*
+         * function to maintain node frequency to a maximum of two in a given linked list
+         * 
+         */
+        public static LinkedList<char> RefineDuplicates(LinkedList<char> MyList)
+        {
+            Dictionary<char, int> CountedNodes = new Dictionary<char, int> { };
+
+            LinkedListNode<char> CurrentNode = MyList.First;
+
+            int MaxCount = 2;
+
+            while (CurrentNode != null)
+            {
+                if (!CountedNodes.ContainsKey(CurrentNode.Value))
+                {
+                    CountedNodes.Add(CurrentNode.Value, 1);
+                }
+                else if (CountedNodes[CurrentNode.Value] < MaxCount)
+                {
+                    CountedNodes[CurrentNode.Value] = CountedNodes[CurrentNode.Value] + 1;
+                }
+                else
+                {
+                    MyList.Remove(CurrentNode.Value);
+                }
+
+                CurrentNode = CurrentNode.Next;
+            }
+            return MyList;
+        }
+    }
+}
